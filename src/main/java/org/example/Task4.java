7f0fea4f8f7f759bf2b6db6cf3ed485e96622673
serogina <seroginadev@gmail.com>
2024-06-25 19:55:40 +0300

    correction in task 4

--- src/main/java/org/example/Task4.java
+++ src/main/java/org/example/Task4.java
@@ -4,7 +4,6 @@ import java.util.stream.Stream;
 
 public class Task4 {
     public static Stream<Long> generateRandomStream(long seed, long a, long c, long m) {
-        return Stream.iterate(seed, xn -> (a * xn + c) % m)
-                .map(Long::valueOf);
+        return Stream.iterate(seed, xn -> (a * xn + c) % m);
     }
 }
